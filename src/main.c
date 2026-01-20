2fbc1def13c687393d3e0dcbccbf4df824eaf359
amirqadimi <amirghadimi.ch@gmail.com>
2026-01-20 18:20:04 +0100

    change to pd14

--- src/main.c
+++ src/main.c
@@ -14,27 +14,27 @@ int main(void)
     // Enable clock for GPIOD
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 
-    // Configure PD13 as general purpose output
-    GPIOD->MODER &= ~(0x3U << (13U * 2U)); // clear mode bits
-    GPIOD->MODER |=  (0x1U << (13U * 2U)); // set to output mode
+    // Configure PD14 as general purpose output
+    GPIOD->MODER &= ~(0x3U << (14U * 2U)); // clear mode bits
+    GPIOD->MODER |=  (0x1U << (14U * 2U)); // set to output mode
 
     // Output type: push-pull
-    GPIOD->OTYPER &= ~(0x1U << 13U);
+    GPIOD->OTYPER &= ~(0x1U << 14U);
 
     // Output speed: medium
-    GPIOD->OSPEEDR &= ~(0x3U << (13U * 2U));
-    GPIOD->OSPEEDR |=  (0x1U << (13U * 2U));
+    GPIOD->OSPEEDR |=  (0x1U << (14U * 2U));
+    GPIOD->OSPEEDR &= ~(0x3U << (14U * 2U));
 
     // No pull-up, no pull-down
-    GPIOD->PUPDR &= ~(0x3U << (13U * 2U));
+    GPIOD->PUPDR &= ~(0x3U << (14U * 2U));
 
     while (1) {
-        // Set PD13
-        GPIOD->BSRR = (0x1U << 13U);
+        // Set PD14
+        GPIOD->BSRR = (0x1U << 14U);
         delay(LED_DELAY_COUNT);
 
-        // Reset PD13
-        GPIOD->BSRR = (0x1U << (13U + 16U));
+        // Reset PD14
+        GPIOD->BSRR = (0x1U << (14U + 16U));
         delay(LED_DELAY_COUNT);
     }
 
